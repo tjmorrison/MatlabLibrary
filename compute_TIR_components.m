9f0ffa891c692459ceab8ed0bc88ef038bd8a5bf
tjmorrison <tjmorrison635@gmail.com>
2017-06-14 11:20:33 -0600

    Update

--- compute_TIR_components.m
+++ compute_TIR_components.m
@@ -1,6 +1,14 @@
 function [T_fluct, T_patch, T_mean] = compute_TIR_components(T,avg_window)
 %make sure num_chunks is 
+
+%Establish vars for easy math 
+nx = size(T,1);
+ny = size(T,2);
 nt = size(T,3);
+n_tot = nt*ny*nx;
+T_vector = reshape(T, [1,n_tot]); 
+
+
 num_chunks = nt/avg_window;
 
 index_start = 1;index_end = avg_window;
@@ -8,7 +16,7 @@ for i = 1:num_chunks
     T_mean = mean(mean(mean(T(:,:,index_start:index_end),1),2),3);
 
     index_start = index_end+1;
-    index_end = index_en
+    index_end = index_end + avg_window;
 end
 
 
