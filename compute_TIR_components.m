c485458923f703dec4a31bc7ab08ed892c852e90
tjmorrison <tjmorrison635@gmail.com>
2017-06-19 12:58:58 -0600

    Compute detrending on chunks

--- compute_TIR_components.m
+++ compute_TIR_components.m
@@ -14,29 +14,37 @@ T_fluct = zeros(nx,ny,nt);
 
 
 %Compute mtrend since it's math does not depend on temporal trend
-T_trend = squeeze(mean(mean(T,1),2)) ;
 
 num_chunks = nt/avg_window;
 T_patch = zeros(nx,ny,num_chunks);
+T_mean = zeros(1,num_chunks);
+T_fluct = zeros(nx,ny,nt);
+T_trend = zeros(1,nt);
 index_start = 1;index_end = avg_window;
 for i = 1:num_chunks
     %space and temporal mean
-    T_mean = mean(mean(mean(T(:,:,index_start:index_end),1),2),3);
+    T_mean(i) = mean(mean(mean(T(:,:,index_start:index_end),1),2),3);
     %calc patchiness
-    T_patch(:,:,i) = mean(T(:,:,index_start:index_end),3);
+    T_patch(:,:,i) = mean(T(:,:,index_start:index_end),3) - T_mean(i);
+    %Calc trend
+    T_trend(index_start:index_end)= squeeze(mean(mean(T(:,:,index_start:index_end),1),2)) - T_mean(i);
     %calc the fluctuations for a chunk
     for j = index_start:index_end
-        T_fluct(:,:,j) = T(:,:,j) - T_patch(:,:,i) - T_trend(j) + (T_mean); 
+        T_fluct(:,:,j) = T(:,:,j) - T_patch(:,:,i) - T_mean(i); 
+        
     end
+    for x = 1:nx
+        for y = 1:ny
+            T_fluct(x,y,index_start:index_end) = squeeze(T_fluct(x,y,index_start:index_end))' - T_trend(1,index_start:index_end);
+        end
+    end
+    
     %increase index
- 
     Percent_complete = ((i*avg_window)/nt)*100
     index_start = index_end+1;
     index_end = index_end + avg_window;
 end
 
 %Calc T_patch over whole time
-T_patch = squeeze(mean(T_patch,3));
-
-
+%T_patch = squeeze(mean(T_patch,3));
 end
