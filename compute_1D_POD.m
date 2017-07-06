d960e229a28476d7b1fd215038c64b18891e781b
tjmorrison <tjmorrison635@gmail.com>
2017-07-06 17:52:22 -0600

    Update

--- compute_1D_POD.m
+++ compute_1D_POD.m
@@ -1,4 +1,4 @@
-function [ phiU,lam ] = compute_1D_POD( var)
+function [ phiU,lam ] = compute_1D_POD( var,figure)
 % METHOD OF SNAPSHOTS
 % calculate the empirical correlation matrix C
 nx = size(var,1);
@@ -28,27 +28,32 @@ lam(end)=0;
 phiU = reshape(fliplr(phi(1:nx,:)),nx,nsnap);
 
 % Plotting the results
-figure()
-%set(h, 'Visible', 'off')
-for i = 1:10
-    plot(phiU(:,i)); 
-    %shading interp;
-    pause(1)
-    savefig(strcat('POD_mode',num2str(i),'.fig'))
+switch figure
+    case 'on'
+        figure()
+        %set(h, 'Visible', 'off')
+        for i = 1:10
+            plot(phiU(:,i)); 
+            %shading interp;
+            pause(1)
+            savefig(strcat('POD_mode',num2str(i),'.fig'))
 
+        end
+
+        figure(1)
+        semilogx(lam(1:end))
+        ylabel('$\lambda$','interpreter','latex','fontsize',20)
+        xlabel('n','interpreter','latex','fontsize',20)
+        savefig('lambda.fig')
+
+        figure()
+        semilogx(lam(1:end)./sum(lam(1:end)))
+        ylabel('$\lambda / \sum{\lambda}$','interpreter','latex','fontsize',20)
+        xlabel('n','interpreter','latex','fontsize',20)
+        savefig('norm_lambda.fig')
+    case 'off'
 end
 
-figure()
-semilogx(lam(1:end))
-ylabel('$\lambda$','interpreter','latex','fontsize',20)
-xlabel('n','interpreter','latex','fontsize',20)
-savefig('lambda.fig')
-
-figure()
-semilogx(lam(1:end)./sum(lam(1:end)))
-ylabel('$\lambda / \sum{\lambda}$','interpreter','latex','fontsize',20)
-xlabel('n','interpreter','latex','fontsize',20)
-savefig('norm_lambda.fig')
 
 end
 
