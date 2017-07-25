ef76e62180cd56ba5180f29dba6256d019d6aef9
tjmorrison <tjmorrison635@gmail.com>
2017-07-25 13:26:09 -0600

    Update

--- compute_1D_POD.m
+++ compute_1D_POD.m
@@ -31,26 +31,65 @@ phiU = reshape(fliplr(phi(1:nx,:)),nx,nsnap);
 switch figure
     case 'on'
         figure()
-        %set(h, 'Visible', 'off')
-        for i = 1:10
-            plot(phiU(:,i)); 
-            %shading interp;
-            pause(1)
-            savefig(strcat('POD_mode',num2str(i),'.fig'))
+        subplot(5,2,1)
+        plot(phiU(:,1),'k-')
+        title('Mode 1', 'interpreter','latex')
+        
+        subplot(5,2,2)
+        plot(phiU(:,2),'k-')
+        title('Mode 2', 'interpreter','latex')
+        
+        
+        subplot(5,2,3)
+        plot(phiU(:,3),'k-')
+        title('Mode 3', 'interpreter','latex')
+        
+        subplot(5,2,4)
+        plot(phiU(:,4),'k-')
+        title('Mode 4', 'interpreter','latex')
+        
+        subplot(5,2,5)
+        plot(phiU(:,5),'k-')
+        title('Mode 5', 'interpreter','latex')
+        
+        subplot(5,2,6)
+        plot(phiU(:,6),'k-')
+        title('Mode 6', 'interpreter','latex')
+        
+        subplot(5,2,7)
+        plot(phiU(:,7),'k-')
+        title('Mode 7', 'interpreter','latex')
+        
+        subplot(5,2,8)
+        plot(phiU(:,8),'k-')
+        title('Mode 8', 'interpreter','latex')
+        
+        subplot(5,2,9)
+        plot(phiU(:,9),'k-')
+        title('Mode 9', 'interpreter','latex')
+        
+        subplot(5,2,10)
+        plot(phiU(:,10),'k-')
+        title('Mode 10', 'interpreter','latex')
+        
+        savefig('POD_modes')
+        saveas(gcf,'POD_modes.png')
 
-        end
-
+        
         figure(1)
         semilogx(lam(1:end))
         ylabel('$\lambda$','interpreter','latex','fontsize',20)
         xlabel('n','interpreter','latex','fontsize',20)
-        savefig('lambda.fig')
-
+        savefig('lambda')
+        saveas(gcf,'lamda.png')
+        
         figure()
-        semilogx(lam(1:end)./sum(lam(1:end)))
+        semilogx(lam(1:end)./sum(lam(1:end)),'k:o')
         ylabel('$\lambda / \sum{\lambda}$','interpreter','latex','fontsize',20)
         xlabel('n','interpreter','latex','fontsize',20)
-        savefig('norm_lambda.fig')
+        savefig('norm_lambda')
+        saveas(gcf,'norm_lamda.png')
+        
     case 'off'
 end
 
